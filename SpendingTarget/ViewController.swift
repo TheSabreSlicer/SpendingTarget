a99051ce60dfde08993799048260af0c816046b7
Connick <connickshields@gmail.com>
2015-10-24 01:38:15 -0700

    Initial Commit

---
+++ SpendingTarget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpendingTarget
+//
+//  Created by Connick Shields on 10/24/15.
+//  Copyright (c) 2015 CLM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
